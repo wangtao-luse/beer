a6c4b9499c404d0664655011f1917f36125ea0b6
wwang-luse <wwangtaoc11@163.com>
2019-11-27 17:32:25 +0800

    beer

--- beer/js/index.js
+++ beer/js/index.js
@@ -1,13 +1,15 @@
 $(function(){
+	//左侧菜单
 	$("#b_cate .cate_menu_item").mouseenter(function(){
         var index=$(this).attr("data-index");
-		$("#b_popCtn").css("display","block");		
+        $(this).addClass("cate_menu_item_on");
+		$("#b_popCtn").css("display","block");
+		$("#b_popCtn .cate_part").css("display","none");
 		$("#cate_item"+index).css("display","block");
-	});
-	
+	});	
 	$("#b_cate .cate_menu_item").mouseleave(function(){
-		var index=$(this).attr("data-index");		
-		$("#cate_item"+index).css("display","none");
+		var index=$(this).attr("data-index");
+		$(this).removeClass("cate_menu_item_on");
 		$("#b_popCtn").css("display","none");
 	});
 	$("#b_popCtn").hover(function(){
