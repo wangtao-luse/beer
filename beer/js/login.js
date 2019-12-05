83c0d2c248093c3d504ebc5e50de14579fb7b14f
wwang-luse <wwangtaoc11@163.com>
2019-12-05 16:08:25 +0800

    login页面

---
+++ beer/js/login.js
@@ -0,0 +1,29 @@
+$(function(){
+	$(".login-tab.login-tab-l").click(function(){
+		$(".login-box").css("display","none");
+		$(".login-form .login-box").css("visibility","hidden");
+		$(".form .item-fore2").css("visibility","hidden");
+		$(".qrcode-login").css("display","block");
+		$(".login-tab a").removeClass("checked");
+		$(this).find("a").addClass("checked");
+	});
+	$(".login-tab.login-tab-r").click(function(){
+		$(".qrcode-login").css("display","none");
+		$(".login-box").css("display","block");
+		$(".login-form .login-box").css("visibility","visible");
+		$(".form .item-fore2").css("visibility","visible");
+		$(".login-tab a").removeClass("checked");
+		$(this).find("a").addClass("checked");
+	});
+	
+    $(".qrcode-img").hover(function(){
+    	$(".qrcode-help").css("display","block");
+    	$(this).css("left","0");
+    	$("#qrcode-error-2016").css("left","-64px");
+    })
+    $(".qrcode-img").mouseleave(function(){
+    	$(".qrcode-help").css("display","none");
+    	$(this).css("left","64px");
+    	$("#qrcode-error-2016").css("left","0px");
+    })
+})
