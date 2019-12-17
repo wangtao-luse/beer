644edebdcbb7fd882e1fcbb441a4d741b4b73a3a
wangtao-luse <wwangtaoc11@163.com>
2019-12-17 23:02:49 +0800

    屏幕适配首页

--- beer/js/index.js
+++ beer/js/index.js
@@ -1,4 +1,21 @@
 $(function(){
+	//https://blog.csdn.net/LEE18254290736/article/details/81326490
+	//https://blog.csdn.net/ChibiMarukoChan/article/details/77934248
+	var htmlObj=$("html");
+	var dpi=window.screen.height;
+	var width=window.screen.width;
+	console.log("dpi:"+dpi);
+	console.log("width:"+width);
+	if(dpi<=1080){
+		htmlObj.addClass("o2_mini");
+	}
+	
+	
+	
+	
+	
+	
+	
 	$("#b_cate .cate_menu_item").mouseenter(function(){
         var index=$(this).attr("data-index");
         $(this).addClass("cate_menu_item_on");
@@ -96,4 +113,5 @@ $(function(){
        $("#b_niceGoods .goods-list").mouseleave(function(){
        	  setTime=setInterval(goods,100);
        });
+       
 });
